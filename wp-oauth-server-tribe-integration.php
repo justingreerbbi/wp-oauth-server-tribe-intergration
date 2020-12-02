d38732798885aa7360843e68dfa720d3a67ed474
Justin Greer <justin@justin-greer.com>
2020-12-02 17:07:19 -0500

    Update wp-oauth-server-tribe-integration.php

--- wp-oauth-server-tribe-integration.php
+++ wp-oauth-server-tribe-integration.php
@@ -1,14 +1,13 @@
 <?php
 /**
  * Plugin Name: WP OAuth Server - Tribe Integration
- * Plugin URI: https://github.com/justingreerbbi/Starter-WordPress-Plugin
- * Description: A starter plugin for WordPress. Get Started Fast!!!
- * Author: justingreerbbi
+ * Plugin URI: https://github.com/justingreerbbi/wp-oauth-server-tribe-intergration
+ * Description: Provides user info resource endpoint data for tribe integration.
+ * Author: Dash10 Digital
  * Version: 0.1.0
  * Author URI: https://dash10.digital
- * Text Domain: starter-plugin
+ * Text Domain: Dash10 Digital
  */
-
 
 define( 'WPOS_TRIBE_INTEGRATION_FILE', plugin_dir_path( __FILE__ ) );
 
